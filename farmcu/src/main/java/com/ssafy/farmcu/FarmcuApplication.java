88fe0a7bc2b2dde443e69353c461ae1e9177c6fc
lab.ssafy <jieun3181@gmail.com>
2023-02-01 22:44:50 +0900

    021

---
+++ farmcu/src/main/java/com/ssafy/farmcu/FarmcuApplication.java
@@ -0,0 +1,14 @@
+package com.ssafy.farmcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FarmcuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmcuApplication.class, args);
+	}
+
+}

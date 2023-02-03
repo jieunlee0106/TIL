94ca0989e185f23e1068c758b81609e4855b6526
JieunLee <jieun3181@gmail.com>
2023-02-03 17:31:36 +0900

    0203

---
+++ back/farmcu/src/main/java/com/ssafy/farmcu/config/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.ssafy.farmcu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig implements WebMvcConfigurer {
+    private ApiInfo commonInfo() {
+        return new ApiInfoBuilder()
+                .title("FarmComeUs API")
+                //.description("")
+                //.license("leeys")
+                //.licenseUrl("http://leeys.tistory.com")
+                .version("1.0")
+                .build();
+    }
+
+    @Bean
+    public Docket allApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("farmcu")
+                .useDefaultResponseMessages(false)
+                .select()
+                //.apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.ssafy.farmcu"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(commonInfo());
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) { //spring-security와 연결할 때 이 부분을 작성하지 않으면 404에러가 뜬다.
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}

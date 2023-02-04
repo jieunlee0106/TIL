c575d6f0116e6fa39fb3252c0d72afb07ebdd3cb
jieunlee0106 <jieun3181@gmail.com>
2023-02-04 23:07:23 +0900

    dd

--- back/farmcu/src/main/java/com/ssafy/farmcu/exception/NotFoundStoreException.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/exception/NotFoundStoreException.java
@@ -2,6 +2,8 @@ package com.ssafy.farmcu.exception;
 
 public class NotFoundStoreException extends RuntimeException{
     public NotFoundStoreException(String message) {
+
+
         System.out.println(message);
     }
 }

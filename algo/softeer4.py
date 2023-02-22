285b491c9b3e5caa2f36b9bd424d38f26f6df5dc
JieunLee <jieun3181@gmail.com>
2023-02-22 17:44:23 +0900

    plus

---
+++ algo/softeer4.py
@@ -0,0 +1,8 @@
+arr = list(map(int, input().split()))
+
+ascending = [1, 2, 3, 4, 5, 6, 7, 8]
+descending = [8, 7, 6, 5, 4, 3, 2, 1]
+
+if arr == ascending:
+    print('ascending')
+elif arr == descending:

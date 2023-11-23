73dff216e6a62359ad528ebbb89c963f01561547
jieunlee0106 <109321523+jieunlee0106@users.noreply.github.com>
2023-11-23 16:21:09 +0900

    프로그래머스_신고 결과 받기 | Lv.1

--- algo/프로그래머스_신고 결과 받기.py
+++ algo/프로그래머스_신고 결과 받기.py
@@ -28,3 +28,20 @@ def solution(id_list, report, k):
                 cnt += 1
         answer.append(cnt)
     return answer
+
+
+
+# 타 풀이
+
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
+    return answer
